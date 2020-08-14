6247f9f0fb9e908fbc9c383217b4e5ae5ae26edd
Ailton Peron Junior <junior.peron@hotmail.com>
2020-08-14 12:57:45 -0300

    feat(app): added initials files for app

---
+++ app/src/app/pages/main/main.page.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+
+//a classe que representa a pagina inicial da aplicação
+
+@Component({
+  selector: 'bird-main',
+  templateUrl: './main.page.html',
+  styleUrls: ['./main.page.scss'],
+})
+export class MainPage implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
